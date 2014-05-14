b95b4a05344e052cc56fd4f144365d1d3b38cf1b
Mateo Acebedo <ma698@cornell.edu>
2014-05-13 23:36:23 -0400

    deleted console statement

--- js/update_location.js
+++ js/update_location.js
@@ -232,7 +232,6 @@ function get_data_for_linegraph(){
                     //var y = parseInt(new_data[0].level);
                     var j = new_data1.length - 20;
                     for (i = -19; i <= 0; i++) {
-                      console.log("j: " + j);
                       var y = 0;
                       if(j >= 0){
                         y = parseInt(new_data1[j].level);
@@ -279,7 +278,6 @@ function get_data_for_linegraph(){
                     //var y = parseInt(new_data[0].level);
                     var j = new_data3.length - 20;
                     for (i = -19; i <= 0; i++) {
-                      console.log("j: " + j);
                       var y = 0;
                       if(j >= 0){
                         y = parseInt(new_data3[j].level);
